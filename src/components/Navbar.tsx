7e688fafd5ddd1eb8f54e7509cb51646816d8255
decheine <decheine@wisc.edu>
2023-07-18 17:44:00 -0500

    Navbar tweak

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,7 +42,7 @@ import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+    const [button, setButton] = useState(false);
 
     var initialWidth = 1920
     var initialHeight = 1080
