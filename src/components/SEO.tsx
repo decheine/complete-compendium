ef87b2f6a8a4b0a20efcf9b4488b81fb2505715e
decheine <decheine@wisc.edu>
2023-07-24 19:28:09 -0500

    SEO Update site_title

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -8,16 +8,18 @@ export const SEO = (props: PropsWithChildren<SEOprops>) => {
 
   return (
     <>
+          <title>AD&D 2nd Edition Complete Monstrous Compendium</title>
           <meta property="og:title" content="AD&D 2nd Edition Complete Monstrous Compendium"/>
+          <meta property="og:site_name" content="Complete Compendium"/>
           <meta property="og:description" content="Complete browsable, searchable appendix of EVERY monster, sourcebook, manual, and campaign setting in Advanced Dungeons & Dragons 2nd Edition."/>
-          <meta property="og:url" content="https://completecompendium.com"/>
-          <meta property="og:image" content="https://completecompendium.com/opengraph_banner.webp"/>
+          <meta property="og:url" content="https://www.completecompendium.com"/>
+          <meta property="og:image" content="https://www.completecompendium.com/opengraph_banner.webp"/>
           <meta property="og:image:width" content="1200"/>
           <meta property="og:image:height" content="630"/>
 
           <meta property="og:type" content="website"/>
           <meta name="twitter:card" content="summary_large_image"/>
-          <meta name="twitter:image" content="https://completecompendium.com/opengraph_banner.webp"/>
+          <meta name="twitter:image" content="https://www.completecompendium.com/opengraph_banner.webp"/>
 
           <meta name="twitter:site" content="@completecompendium" />
           <meta name="twitter:title" content="Advanced Dungeons & Dragons 2nd Edition Complete Compendium" />
