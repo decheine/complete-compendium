0ba0e11ce79a074744054484c4f16f6a6cbba34e
decheine <decheine@wisc.edu>
2023-12-08 19:03:35 -0600

    update  script

--- scripts/copy_monster_data.js
+++ scripts/copy_monster_data.js
@@ -1,6 +1,7 @@
 //copyfile.js
 const fs = require('fs');
 
+
 // destination will be created or overwritten by default.
 // output\json_files\AA_ALL_MONSTERS.json
 fs.copyFile('output/json_files/AA_ALL_MONSTERS.json', 'src/data/ALL_MONSTERS.json', (err) => {
@@ -16,4 +17,20 @@ fs.copyFile('output/json_files/AA_Full_Catalog.json', 'src/data/Full_Catalog.jso
 fs.copyFile('output/json_files/AA_KEY_TITLES.json', 'src/data/AA_KEYS_TITLES.json', (err) => {
   if (err) throw err;
   console.log('Keys to Titles data was copied to destination');
+});
+
+// Create publish_id to setting_acr
+// read in all_tsr
+const all_tsr = require('../data/all_tsr.json')
+const settingsTitletoAcronym = require('../src/data/SettingTitleToAcronym.json')
+let publishId_to_acronym = {}
+Object.entries(all_tsr).map(publish_id => {
+  // console.log(publish_id[0], settingsTitletoAcronym[publish_id[1].setting])
+  publishId_to_acronym[publish_id[0]] = settingsTitletoAcronym[publish_id[1].setting]
+})
+
+fs.writeFile("./src/data/publishId_to_acronym.json", JSON.stringify(publishId_to_acronym), function(err) {
+  if (err) {
+      console.log(err);
+  }
 });
