b4424b1a6c8909eb639bbdc6aac790a566bc1fa9
decheine <decheine@wisc.edu>
2024-01-17 17:14:20 -0600

    Settings monster list

--- src/templates/setting.tsx
+++ src/templates/setting.tsx
@@ -6,9 +6,17 @@ import Layout from "@components/Layout";
 // import * as settingStyles from "@styles/modules/settings.module.css"
 
 import { BookList } from "@components/BookList";
+import { Link } from "gatsby";
+
+import * as bookStyles from '@styles/modules/book.module.css'
+
+
 // import './SettingPage.css'
 const ACRONYMS = require('@data/CatAcronyms.json')
 const AcronymToSetting = require('@data/AcronymsSetting.json')
+const KeyToTitles = require('@data/AA_KEYS_TITLES.json')
+const settings = require("../../data/settings.json")
+const Catalog = require('@data/Full_Catalog.json')
 
 // Input Structure
 interface CategoryProps {
@@ -60,8 +68,75 @@ type SettingDataWrapper = {
 
 interface Props {
     pageContext: SettingDataWrapper
-  }
+}
 
+interface BookMonstersProps {
+    setting_key: string
+}
+
+type setting_type = {
+    setting_key: string,
+    setting_name: string,
+    source_books: Array<string>
+}
+
+type catalog_entry = {
+    author: string,
+    monster_keys: string[],
+    publish_id: string,
+    setting: string,
+    title: string,
+    year: string
+}
+
+
+// List of monster
+function BookMonstersList(props: BookMonstersProps) {
+    console.log(props)
+    let setting_books: string[] = []
+    settings.forEach((target_setting: setting_type) => {
+        if (target_setting.setting_key == props.setting_key) {
+            setting_books = target_setting.source_books
+        }
+    })
+    console.log(setting_books)
+    let setting_monsters: string[] = []
+    let setting_book_monsters = {}
+    let monster_links: JSX.Element[] = []
+
+    
+
+
+    Catalog.forEach((book: catalog_entry) => {
+        if (setting_books.includes(book.publish_id)){
+            // append this books mosnters to setting_monsters
+            setting_book_monsters[book.publish_id] = book.monster_keys
+        }
+    })
+    console.log("Setting book monsters")
+    
+    console.log(setting_book_monsters)
+
+    setting_books.forEach((publish_id: string) => {
+        console.log(publish_id)
+        console.log(setting_book_monsters[publish_id])
+        setting_book_monsters[publish_id].forEach((monster_key: string) => {
+            monster_links = monster_links.concat(
+                <>
+                <Link to={'/catalog/' + props.setting_key + "/" + publish_id + "/" + monster_key} className={bookStyles.listLink} key={monster_key}>
+                          <div className={bookStyles.listEntry} >
+                              {KeyToTitles[monster_key]}
+                          </div>
+                          </Link>
+                </>
+            )
+        });
+    })
+
+
+    return monster_links
+
+}
 
 
 const SettingTemplate: React.FC<Props> = ({ pageContext }) => {
@@ -81,10 +156,12 @@ const SettingTemplate: React.FC<Props> = ({ pageContext }) => {
                     {/* <div className={settingStyles.settingLogo}> */}
                     <div style={settingImgStyle}>
                         <img style={headerImgStyle} src={'/images/grf/' +  pageContext.category + '.gif'} alt={AcronymToSetting[pageContext.category] + 'category image'}></img>
-
                     </div>
                     
                     <BookList category={pageContext.category}/>
+                    List of all monsters across these books.
+                    <BookMonstersList setting_key={pageContext.category}/>
+
                 </Layout>
             </>
         )
